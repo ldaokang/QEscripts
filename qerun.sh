707d7bd19c7e9c05dd11c2c60727bd73c6b61e20
Mike <ldaokang@gmail.com>
2017-05-06 15:33:10 +0800

    Add files via upload

--- qerun.sh
+++ qerun.sh
@@ -1,35 +1,105 @@
-#QERUN.SH 2017 by Michael Lee
-#TURNS XYZ FILES INTO INPUT FILES & EXECUTES (OPTIONAL)
-update_elem () { #update this function depending on elements used
+#QERUN.SH V0 by Michael Lee
+#INPUT: *.xyz
+#OUPUT: *.in & *pbs (with option to qsub)
+
+update_elem () {
 	case $1 in
 		'H')
+			pseudopot=""
 			val=1;;
 		'C')
 			pseudopot="C 12.0107 C.pbe-n-rrkjus_psl.0.1.UPF"
 			val=4;;
 		'O')
+			pseudopot=""
 			val=6;;
 		'F')
 			pseudopot="F 18.99840325 F.pbe-n-rrkjus_psl.0.1.UPF"
 			val=7;;
 		'Pt')
+			pseudopot=""
 			val=10;;
 		*)
 			echo "Invalid Element" $1
 	esac
 }
 
-rm_filelist (){
+printparams () {
+	echo "Writing $prefix.in with parameters:"
+	printf "NATOM: $NATOM \n"
+	printf "NTYP: $NTYP \n"
+	printf "NBND: $NBND \n"
+	printf "ecutwfc = %.1f \n" $ecutwfc
+	printf "ecutrho = %.1f \n" $ecutrho
+	printf "ecutrho = %.1g \n" $degauss
+	printf "KPOINTS: %s \n" "${kpoints[@]}"
+}
+
+rm_filelist (){ #rm .tmp files
 	rm NAT_TYP_ARR.tmp
 }
 
-while read prefix
+GLOBAL_ECUTWFC=40.0
+GLOBAL_ECUTRHO=480.0
+GLOBAL_DEGAUSS=0.001
+GLOBAL_KPOINTS=(18 18 1 0 0 0)
+while read -r -u "$pf" prefix
 do
+	ecutwfc=$GLOBAL_ECUTWFC
+	ecutrho=$GLOBAL_ECUTRHO
+	degauss=$GLOBAL_DEGAUSS
+	kpoints=${GLOBAL_KPOINTS[@]}
+	pseudopot=""
+	val=-1
 	INPUT_FILE=$(printf $prefix".in")
 	>$INPUT_FILE
 	PBS_FILE=$(printf "run-"$prefix".pbs")
 	>$PBS_FILE
 	XYZ=$(printf $prefix".xyz")
+	
+	if [ ! -f $XYZ ] #Check if XYZ file exists
+	then
+		echo "$XYZ not found. Terminating job."
+		continue
+	else
+		echo "Analysing $XYZ..."
+	fi
+
+	if [ ! -d $prefix ] #Check if prefix directory exists
+	then
+		echo "Subdirectory $prefix not found"
+		read -r -p "mkdir $prefix [m], run here[h]? Press any other key to skip run" reply
+		case "$reply" in
+			[mM])
+				echo "Making subdirectory $prefix"
+				mkdir $prefix
+				cd $prefix
+				cp ../$XYZ .
+				;;
+			[hH])
+				echo "Running $prefix in this folder";;
+			*)
+				echo "Skipping $prefix"
+				continue;;
+		esac
+	else
+		cd $prefix
+		if [ -f $INPUT_FILE ] #Check if input file exists
+		then
+			read -r -p "Replace $INPUT_FILE?[y/n]" reply
+			case $reply in
+				[yY])
+				        echo "Overwriting $INPUT_FILE"	
+					rm $INPUT_FILE;;
+				*) 
+					echo "Skipping $prefix"
+					cd ..
+					continue;;
+			esac
+		fi
+		cp ../$XYZ .
+	fi
+#START XYZ ANALYSIS
 	tail -n+3 $XYZ | cut -c1-2 | sort | uniq -c > NAT_TYP_ARR.tmp #array of elements
 	TYP_ARR=($(cut -c9-10 NAT_TYP_ARR.tmp))  #array of elements
 	NATOM_ARR=($(cut -c6-7 NAT_TYP_ARR.tmp))  #array of number of atoms
@@ -37,20 +107,48 @@ do
 	NATOM=$(head -n1 $XYZ | tr -d '\r') #can check with sum atoms
 	NBND=0 
 	PSEUDO_FILE="pseudo-template"
+	NATOM_CHK=0
 	elem_index=0
 	for i in "${TYP_ARR[@]}"
 	do
 		#PSEUDO_POTS[$elem_index]=$(grep -w $i $PSEUDO_FILE) #match whole words (not 'F' in 'UPF')
 		update_elem $i
 		NBND=$(($NBND+(${NATOM_ARR[$elem_index]}*val+1)/2))
+		NATOM_CHK=$(($NATOM_CHK+${NATOM_ARR[$elem_index]}))
 		elem_index=$(($elem_index+1))
 		PSEUDO_POTS[$elem_index]=$pseudopot #match whole words (not 'F' in 'UPF')
 	done
 	NBND=$(($NBND+10)) #buffer NBND by 10
-
-#SUBSTITUTION & PRINT
-#TO INSERT NEW VARIABLE, BREAK PRINTF STREAM & PRINTF AT DESIRED LINE
-#PRINT TO INPUT_FILE
+#END XYZ ANLAYSIS
+	if [ ! $NATOM_CHK -eq $NATOM ] #Check no. of atoms
+	then
+		echo "Number of atoms in $XYZ is wrong. Skipping $prefix..."
+		continue
+	fi
+	EDIT_PARAMS="y"
+	while true
+	do
+		printparams
+		read -r -p "Change: ecutwfc/ecutrho[e] K-points[k] degauss[d]? Press any other key to continue" reply
+	       	case "$reply" in
+			[eE]) 
+				read -r -p "Enter new value for ecutwfc: " ecutwfc
+				read -r -p "Enter new value for ecutrho: " ecutrho
+				read -r -p "Apply to all files?[y/n]" yn
+				if [ yn = "y" ]
+				then GLOBAL_ECUTWFC=$ecutwfc;GLOBAL_ECUTRHO=$ecutrho
+				fi;;
+			[dD]) 
+				read -r -p "Enter new value for degauss: " degauss
+				read -r -p "Apply to all files?[y/n]" yn
+				if [ yn = "y" ]
+				then GLOBAL_DEGAUSS=$degauss
+				fi;;
+			[kK]) 
+				read -r -p "Enter new values for K Points: " kpoints;;
+			*) break;;
+		esac	
+	done
 printf "&control
     calculation = 'relax'
     restart_mode='from_scratch'
@@ -61,14 +159,14 @@ printf "&control
  &system
    ibrav = 12, celldm(1) = 27.8923581741617, celldm(2) = 1.0 , celldm(3) = 1.0
     celldm(4) = -0.5, celldm(5) = 0.0, celldm(6) = 0.0
-    ecutwfc = 40.0
-    ecutrho = 480.0
+    ecutwfc = %.1f
+    ecutrho = %.1f
     nat = %i
     ntyp = %i 
     nbnd = %i
     occupations = 'smearing'
     smearing = 'cold'
-    degauss = 0.001
+    degauss = %.1g
     nspin = 2
     starting_magnetization = 1.0
  /
@@ -84,15 +182,16 @@ printf "&control
  pot_extrapolation = 'second order'
  /
  ATOMIC_SPECIES
-" $prefix $prefix $NATOM $NTYP $NBND >> $INPUT_FILE #print main parameters
+" $prefix $prefix $ecutwfc $ecutrho $NATOM $NTYP $NBND $degauss >> $INPUT_FILE #print main parameters
 	printf "%s\n" "${PSEUDO_POTS[@]}" >> $INPUT_FILE #print pseudopotentials
 	printf "ATOMIC_POSITIONS (angstrom)\n" >> $INPUT_FILE
 	tail -n+3 $XYZ | tr -d '\r' >> $INPUT_FILE #print coordinates, fixes non-unix endline escape key
-	printf "K_POINTS (automatic) \n18 18 1 0 0 0" >> $INPUT_FILE
+	printf "\nK_POINTS (automatic) \n%s\n" "${kpoints[@]}" >> $INPUT_FILE
 
 #PRINT TO PBS_FILE
+echo "PRINTING $PBS_FILE"
 printf "#!/bin/sh
-#PBS -N QuantumEspresso
+#PBS -N %s
 # ask for 3 chunks of 24 cores, flat MPI (48 processes in total)
 #PBS -l select=3:ncpus=24:mpiprocs=24:ompthreads=1:mem=96gb
 # modify for required runtime
@@ -118,7 +217,14 @@ cd \${PBS_O_WORKDIR}
 mpirun pw.x -i \$inputfile > output-%s.\$PBS_JOBID
 
 # all files will now be copied back into location specified by stageout setting
-" $INPUT_FILE $prefix >> $PBS_FILE
-
+" $prefix $INPUT_FILE $prefix >> $PBS_FILE
+read -r -p "qsub $PBS_FILE?[y/n]" yn
+if [ yn = "y" ]
+then
+	echo "Submitting $PBS_FILE"
+	qsub $PBS_FILE > jobid
+fi
 rm_filelist
-done < prefixes
+cd ..
+printf "\n"
+done {pf}<prefixes
